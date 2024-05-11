500aa28916cf392e52eb30dca9f2adc70c98b58f
CoderAscia0277 <CoderAscia0277@users.noreply.github.com>
2024-05-11 20:53:25 +0800

    I redesign the emoji toggler icon into a more option icon I redesign the menu option icon located on the upper right corner into a menu icon with blue background I added a new panel logic toggler that allows me to pick multiple option e.g you wanna select sticker press icon emoji, if you wanna go to other maps you may choose on the second icon which displays the available map to explore. Etc. Balak ko kasi gamit ung more option panel mas maging interactive ung chat system not only you can chat with them but also can change maps and adjust the time , stats etc.

--- src/chat/core/chatIndex.jsx
+++ src/chat/core/chatIndex.jsx
@@ -123,6 +123,12 @@ const ChatIndex = () => {
         
     }
 
+    const EmojiRenderer = ({image = "..."}) => {
+        return(
+            <img className="w-1/4 h-full rounded-lg hover:scale-105 cursor-pointer" alt="..." src={image} style={{aspectRatio:1/1, background:theme.light }}  />
+                               
+        );
+    }
     return(
         <section className="lg:w-2/6 md:w-2/5 w-full lg:3/4 md:3/4 h-full absolute xs:left-0  lg:top-0 md:top-0 bottom-0  lg:rounded-xl md:rounded-xl  mt-0 flex flex-col pt-2 " style={{background:'url(./images/classroom_bg.jpg) center/cover no-repeat'}}>
             <article className="w-full min-h-16 flex flex-row items-center px-2">
@@ -134,9 +140,12 @@ const ChatIndex = () => {
                         Moe Moe Paradise!
                     </span>
                 </div>
-                <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" className="bi bi-three-dots-vertical text-white w-8 h-8 hover:cursor-pointer hover:scale-110" viewBox="0 0 16 16">
-                    <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" style={{background:theme.light}} class="bi bi-list text-white w-8 h-8 hover:cursor-pointer hover:scale-110 rounded-md " viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
                 </svg>
+                {/* <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" className="bi bi-three-dots-vertical text-white w-8 h-8 hover:cursor-pointer hover:scale-110" viewBox="0 0 16 16">
+                    <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+                </svg> */}
             </article>
             <article id="ScrollView" onClick={() => setEmojiToggle(false)} className="super_parent w-full flex-grow container overflow-y-scroll  pt-4" style={{scrollBehavior:'smooth'}}>
                 {
@@ -150,12 +159,12 @@ const ChatIndex = () => {
                 }
             </article>
             <article className={`w-full absolute bottom-0  z-10 transition-all flex flex-col py-4 px-2  ${!DialogueState ? '' : 'opacity-0 pointer-events-none'}  `} style={{backgroundColor:'rgba( 32, 32, 32, 0.20)',backdropFilter:'blur(1px)'}}>
-                <div className="flex flex-row items-center justify-center gap-4 px-2 ">
-                    <span className={`  ${isTextFieldFocus ? 'w-0 hidden' : 'w-1/6 flex'}  flex-row justify-center items-center gap-4`}>
+                <div className="flex flex-row items-center justify-center gap-3 px-0 ">
+                    <span className={`  ${isTextFieldFocus ? 'w-0 hidden' : 'w-max flex'}  flex-row justify-center items-center gap-4`}>
 
-                        <span className={`rounded-full   overflow-hidden scale-110 ${isTextFieldFocus ? 'p-0 w-0' : 'p-2'}`} style={{background:theme.dark}}>
-                            <svg xmlns="http://www.w3.org/2000/svg" onClick={() => setEmojiToggle(!EmojiSticker)} fill="currentColor" className="bi bi-emoji-wink-fill w-7 h-7 text-neutral-400 text-white hover:cursor-pointer  " viewBox="0 0 16 16">
-                                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M7 6.5C7 5.672 6.552 5 6 5s-1 .672-1 1.5S5.448 8 6 8s1-.672 1-1.5M4.285 9.567a.5.5 0 0 0-.183.683A4.5 4.5 0 0 0 8 12.5a4.5 4.5 0 0 0 3.898-2.25.5.5 0 1 0-.866-.5A3.5 3.5 0 0 1 8 11.5a3.5 3.5 0 0 1-3.032-1.75.5.5 0 0 0-.683-.183m5.152-3.31a.5.5 0 0 0-.874.486c.33.595.958 1.007 1.687 1.007s1.356-.412 1.687-1.007a.5.5 0 0 0-.874-.486.93.93 0 0 1-.813.493.93.93 0 0 1-.813-.493"/>
+                        <span className={`rounded-full   overflow-hidden scale-110 ${isTextFieldFocus ? 'p-0 w-0' : 'p-2'}`} style={{background:EmojiSticker ? theme.light : theme.dark}}>
+                            <svg xmlns="http://www.w3.org/2000/svg" onClick={() => setEmojiToggle(!EmojiSticker)} fill="currentColor" className="bi bi-three-dots w-7 h-7 text-neutral-400 text-white hover:cursor-pointer  " viewBox="0 0 16 16">
+                            <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3"/>
                             </svg>
                         </span>
                     
@@ -176,28 +185,54 @@ const ChatIndex = () => {
                         }
                     </span>
                 </div>
-                <div className={`${EmojiSticker ? 'push_up flex' : 'pull_down'} px-2 pt-2 `}>
-                    <div className="h-full w-full flex flex-col transition-all rounded-2xl " style={{background:theme.dark}}>
-                        <div className=" w-full flex-grow rounded-xl">
-    
+                <div className={`${EmojiSticker ? 'push_up flex' : 'pull_down'} px-0 pt-2 overflow-hidden `}>
+                    <div className="h-full w-full flex flex-col transition-all rounded-md gap-2 pt-2" style={{background:theme.dark}}>
+                        <div className="w-full h-10 px-4 flex flex-grow gap-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-emoji-wink-fill w-7 h-7 text-neutral-400 text-white hover:cursor-pointer  " viewBox="0 0 16 16">
+                                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M7 6.5C7 5.672 6.552 5 6 5s-1 .672-1 1.5S5.448 8 6 8s1-.672 1-1.5M4.285 9.567a.5.5 0 0 0-.183.683A4.5 4.5 0 0 0 8 12.5a4.5 4.5 0 0 0 3.898-2.25.5.5 0 1 0-.866-.5A3.5 3.5 0 0 1 8 11.5a3.5 3.5 0 0 1-3.032-1.75.5.5 0 0 0-.683-.183m5.152-3.31a.5.5 0 0 0-.874.486c.33.595.958 1.007 1.687 1.007s1.356-.412 1.687-1.007a.5.5 0 0 0-.874-.486.93.93 0 0 1-.813.493.93.93 0 0 1-.813-.493"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-emoji-wink-fill w-7 h-7 text-neutral-400 text-white hover:cursor-pointer  " viewBox="0 0 16 16">
+                                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M7 6.5C7 5.672 6.552 5 6 5s-1 .672-1 1.5S5.448 8 6 8s1-.672 1-1.5M4.285 9.567a.5.5 0 0 0-.183.683A4.5 4.5 0 0 0 8 12.5a4.5 4.5 0 0 0 3.898-2.25.5.5 0 1 0-.866-.5A3.5 3.5 0 0 1 8 11.5a3.5 3.5 0 0 1-3.032-1.75.5.5 0 0 0-.683-.183m5.152-3.31a.5.5 0 0 0-.874.486c.33.595.958 1.007 1.687 1.007s1.356-.412 1.687-1.007a.5.5 0 0 0-.874-.486.93.93 0 0 1-.813.493.93.93 0 0 1-.813-.493"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-emoji-wink-fill w-7 h-7 text-neutral-400 text-white hover:cursor-pointer  " viewBox="0 0 16 16">
+                                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M7 6.5C7 5.672 6.552 5 6 5s-1 .672-1 1.5S5.448 8 6 8s1-.672 1-1.5M4.285 9.567a.5.5 0 0 0-.183.683A4.5 4.5 0 0 0 8 12.5a4.5 4.5 0 0 0 3.898-2.25.5.5 0 1 0-.866-.5A3.5 3.5 0 0 1 8 11.5a3.5 3.5 0 0 1-3.032-1.75.5.5 0 0 0-.683-.183m5.152-3.31a.5.5 0 0 0-.874.486c.33.595.958 1.007 1.687 1.007s1.356-.412 1.687-1.007a.5.5 0 0 0-.874-.486.93.93 0 0 1-.813.493.93.93 0 0 1-.813-.493"/>
+                            </svg>
                         </div>
-                    <div className=" w-full flex flex-row h-5 overflow-hidden gap-4 mb-2 justify-center ">
-                        <svg xmlns="http://www.w3.org/2000/svg" onClick={(e) => {e = emoji_indicator_icon_status.map((item,index) => { return index === 0 ?  true : false}); setEmojiIndicator(e)}} fill="currentColor" className={`${circle} ${emoji_indicator_icon_status[0] ? 'text-white' : 'text-neutral-500'}`} viewBox="0 0 16 16">
-                            <circle cx="8" cy="8" r="8"/>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" onClick={(e) => {e = emoji_indicator_icon_status.map((item,index) => {return index === 1 ? true : false}); setEmojiIndicator(e)}}   fill="currentColor" className={`${circle} ${emoji_indicator_icon_status[1] ? 'text-white' : 'text-neutral-500'}`} viewBox="0 0 16 16" >
-                            <circle cx="8" cy="8" r="8"/>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" onClick={(e) => {e = emoji_indicator_icon_status.map((item,index) => {return index === 2 ? true  : false}); setEmojiIndicator(e)}}  fill="currentColor" className={`${circle} ${emoji_indicator_icon_status[2] ? 'text-white' : 'text-neutral-500'}`} viewBox="0 0 16 16" >
-                            <circle cx="8" cy="8" r="8"/>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" onClick={(e) => {e = emoji_indicator_icon_status.map((item,index) => { return index === 3 ? true  : false}); setEmojiIndicator(e)}}   fill="currentColor"className={`${circle} ${emoji_indicator_icon_status[3] ? 'text-white' : 'text-neutral-500'}`} viewBox="0 0 16 16" >
-                            <circle cx="8" cy="8" r="8"/>
-                        </svg>
-               
+                        <div className=" w-full flex-grow flex flex-col gap-4 rounded-xl py-4 overflow-y-auto">
+                            <div className="w-full  flex flex-row gap-4 items-center justify-center">
+                               <EmojiRenderer/>  
+                               <EmojiRenderer/>  
+                               <EmojiRenderer/>  
+                            </div>
+                            <div className="w-full   flex flex-row gap-4 justify-center">
+                                <EmojiRenderer/>  
+                                <EmojiRenderer/>  
+                                <EmojiRenderer/>  
+                            </div>
+
+                        </div>
+                        <div className=" w-full flex flex-row h-10 items-center overflow-hidden gap-4 mb-2 justify-center ">
+                            <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" className="bi bi-caret-left-fill w-6 h-6 text-white cursor-pointer hover:scale-105" viewBox="0 0 16 16">
+                                <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" onClick={(e) => {e = emoji_indicator_icon_status.map((item,index) => { return index === 0 ?  true : false}); setEmojiIndicator(e)}} fill="currentColor" className={`${circle} ${emoji_indicator_icon_status[0] ? 'text-white' : 'text-neutral-500'}`} viewBox="0 0 16 16">
+                                <circle cx="8" cy="8" r="8"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" onClick={(e) => {e = emoji_indicator_icon_status.map((item,index) => {return index === 1 ? true : false}); setEmojiIndicator(e)}}   fill="currentColor" className={`${circle} ${emoji_indicator_icon_status[1] ? 'text-white' : 'text-neutral-500'}`} viewBox="0 0 16 16" >
+                                <circle cx="8" cy="8" r="8"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" onClick={(e) => {e = emoji_indicator_icon_status.map((item,index) => {return index === 2 ? true  : false}); setEmojiIndicator(e)}}  fill="currentColor" className={`${circle} ${emoji_indicator_icon_status[2] ? 'text-white' : 'text-neutral-500'}`} viewBox="0 0 16 16" >
+                                <circle cx="8" cy="8" r="8"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" onClick={(e) => {e = emoji_indicator_icon_status.map((item,index) => { return index === 3 ? true  : false}); setEmojiIndicator(e)}}   fill="currentColor"className={`${circle} ${emoji_indicator_icon_status[3] ? 'text-white' : 'text-neutral-500'}`} viewBox="0 0 16 16" >
+                                <circle cx="8" cy="8" r="8"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-caret-right-fill w-6 h-6 text-white cursor-pointer hover:scale-105" viewBox="0 0 16 16">
+                                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                            </svg>
+                        </div>
                     </div>
                 </div>
-            </div>
                         
                 
      
