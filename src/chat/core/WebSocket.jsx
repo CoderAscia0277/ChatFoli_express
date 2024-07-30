c7cbfd9ea789803338e508bea6a15c4f8ecba208
CoderAscia0277 <CoderAscia0277@users.noreply.github.com>
2024-07-30 10:37:30 +0800

    Im trying to make a simple chat app

--- src/chat/core/WebSocket.jsx
+++ src/chat/core/WebSocket.jsx
@@ -1,19 +1,46 @@
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState,useMemo } from "react";
 
 const ws = new WebSocket('ws://localhost:8080');
 
+const Message_Box = ({option = []}) => {
+    return(
+        <section className="w-1/4 h-3/4 border rounded-md border-neutral-500 absolute bg-neutral-800 flex flex-col" style={{right:'15%'}}>
+            <article className="w-full h-10 p-4 flex flex-row gap-2">
+                <p className="text-neutral-300">Contact:</p>
+                <input list="user_list" className="bg-transparent w-max h-6 outline-0 text-neutral-300 px-2" placeholder="Select here" />
+                <datalist id="user_list">
+                    {
+                        option ?
+                            option.map((item,index)=>{
+                                return <option value={item} key={index}/>
+                            })
+                        : null  
+                    }
+
+                </datalist>
+            </article>
+            <article className="w-full  block flex-grow"></article>
+            <article className="w-full h-16 flex  py-2 px-4">
+                <input type="text" className=" h-10 w-full border bg-neutral-800 border-neutral-700 outline-0 rounded-md text-neutral-300 px-2"/>
+            </article>
+        </section>
+    );
+}
+
 const Sample = () => {
 
-    const [text,set_text] = useState('Websocket');
+    
     const isMounted = useRef(false);
+    const [{id,online,other_id},set_data] = useState({id:'null',online:0,other_id:[]});
 
     ws.onopen = () => {
         // set_text('Connected!');
     };
     ws.onmessage = event => {
         const parse = JSON.parse(event.data);
-        set_text(`${parse.count} | Online : ${parse.online}`);
+        const reverse_arr = parse.other_id.reverse();
+        set_data({...parse,other_id:reverse_arr});
     }
     ws.onerror = err => {
         console.log('Oppss!');
@@ -36,7 +63,27 @@ const Sample = () => {
         
     },[]);
     
-    return(<p>{text}</p>);
+    return(
+        <>
+        <ul>
+            <p className="text-neutral-300">Your unique Id is: {id}</p>
+            <p className="text-neutral-300">Active: {online}</p>
+            <p className="text-neutral-300">List of Online:</p>
+            <li>
+            {
+               other_id.map((name,index) => {
+                        return(<p className={`${name === id ? 'text-neutral-300' : 'text-neutral-700'}`} key={index}>{name}</p>);
+                 })
+
+            }
+            </li>
+
+            
+        </ul>
+        <Message_Box option={other_id}/>
+        </>
+
+    );
 }
 
 export default Sample;
