5627dc038375e21afceceaa7fb00b31a52cd7410
CoderAscia0277 <CoderAscia0277@users.noreply.github.com>
2024-09-26 12:46:11 +0800

    I added a scroll logic which allows me to automatically scroll the container as the AI type its response

--- src/chat/components/ChatApp/MessagingApp.jsx
+++ src/chat/components/ChatApp/MessagingApp.jsx
@@ -47,7 +47,7 @@ const ws = {
 };
 
 
-const SubmitIcon = ({isRequesting}) => {
+const SubmitIcon = ({isRequesting,send = () => null}) => {
     const Theme = useContext(ThemeContext);
     // const isRequesting = useContext(API_STATUS);
 
@@ -55,7 +55,7 @@ const SubmitIcon = ({isRequesting}) => {
         return(<LoadingSpinner size={Theme.IconSize} />)
     }else{
         return(
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={`${Theme.IconSize} hover:scale-110 cursor-pointer`} color="#F8F9FA" fill="none">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" onClick={() =>  send()} className={`${Theme.IconSize} hover:scale-110 cursor-pointer`} color="#F8F9FA" fill="none">
             <path d="M21.0477 3.05293C18.8697 0.707363 2.48648 6.4532 2.50001 8.551C2.51535 10.9299 8.89809 11.6617 10.6672 12.1581C11.7311 12.4565 12.016 12.7625 12.2613 13.8781C13.3723 18.9305 13.9301 21.4435 15.2014 21.4996C17.2278 21.5892 23.1733 5.342 21.0477 3.05293Z" stroke="currentColor" strokeWidth="1.5" />
             <path d="M11.5 12.5L15 9" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
         </svg>);
@@ -82,35 +82,41 @@ const UserTextArea = ({ action = () => null, set_request_state = () => null, isR
         }
     },[isRequesting]);
 
-    const submit_action = async(e) => {
-        e.preventDefault();
-        if(e.target.value){
-            
-            const userInput = e.target.value;
-         
-            action(userInput); // creates new user bubble based on input
 
-            e.target.blur(); //disables focus on the text box
-            e.target.value = ''; //resets the user input
-
-            e.target.readOnly = true;
-
-            set_request_state(true);
-
-            //send_to_AI(userInput);// send messages to the server
-            
-            ws.socket.send(JSON.stringify({'method':'SEND-MESSAGE','message':userInput}));
-            
-          
+    const submit_action = async(e = null) => {
+        let userInput = '';
+        if(e != null){
+            e.preventDefault();
+            if(e.target.value){
+                
+                userInput = e.target.value;
+                e.target.blur(); //disables focus on the text box
+                e.target.value = ''; //resets the user input
+                e.target.readOnly = true;   
+            }
+        }else{
+            userInput = text_field.current.value;
+            text_field.current.blur();
+            text_field.current.value = '';
+            text_field.current.readOnly = true;
         }
+        action(userInput); // creates new user bubble based on input
+        set_request_state(true);
+        // send messages to the server      
+        ws.socket.send(JSON.stringify({'method':'SEND-MESSAGE','message':userInput}));
+                
     }
 
     return(
-       <input type="text"  ref={text_field} onKeyDown={e => e.key === "Enter" ? submit_action(e) : null} className="bg-transparent  flex-grow h-14 outline-0 px-4 text-center text-neutral-100"  placeholder="Please enter your response here." style={{resize:'none'}}/>      
+        <>
+            <input type="text"  ref={text_field} onKeyDown={e => e.key === "Enter" ? submit_action(e) : null} className="bg-transparent  flex-grow h-14 outline-0 px-4 text-center text-neutral-100"  placeholder={`${isRequesting ? "Azumi is currently typing..." :"Please enter your response here."}`} style={{resize:'none'}}/>      
+            <SubmitIcon send={() => submit_action()} isRequesting={isRequesting}/>
+        </>
+       
     );
 };
 
-const Bubble = ({value,response_type,set_request_state = () => null}) => {
+const Bubble = ({scrollUp = () => null,value,response_type,set_request_state = () => null}) => {
     const Theme = useContext(ThemeContext);
     const [streamMessage,update_streamMessage] = useState('');
     // const text_field = useRef(null);
@@ -131,12 +137,12 @@ const Bubble = ({value,response_type,set_request_state = () => null}) => {
                         this.cache.push(text_array[this.cache.length]);
         
                         update_streamMessage(this.cache.join(''));
-        
+
+                        scrollUp(); // Scrolls the messages container everytime the bubble values is being updated
+
                         if(this.cache.length === text_array.length){
                             clearInterval(interval);
                             set_request_state(false);
-                            const scrollView = document.querySelector('.chatContainer');
-                            scrollView.scrollTop = scrollView.scrollHeight;
                         }
                     },30);
                 }
@@ -176,31 +182,24 @@ const Bubble = ({value,response_type,set_request_state = () => null}) => {
     
 };
 
-const MessageScrollView = ({chats}) => {
+const MessageScrollView = () => {
+
+    const Theme = useContext(ThemeContext);
+
+    const [request_state,set_request_state] = useState(false); // Determines if the front end is busy requesting or not
+
+    const ScrollUp = () => {
+        ScrollView.current.scrollTop = ScrollView.current.scrollHeight;
+    };
+
+    const [chat_blocks,update_chat_blocks] = useState([<Bubble key={0} scrollUp={() => ScrollUp()} value={'Izumi-kun eating alone again? *sits next to him*'} response_type={'intro'}/>]);
 
     const ScrollView = useRef(null);
     useEffect(() => {
         ScrollView.current.scrollTop = ScrollView.current.scrollHeight;
-    },[chats]);
+    },[chat_blocks]);
 
-    return(
-        <article ref={ScrollView} className="overflow-y-auto lg:w-3/4 w-full block h-full  ">
-            <div  className="chatContainer w-full h-max rounded-lg p-1 flex flex-col-reverse px-4 gap-8  " style={{background:''}} >
-                {chats}
-            </div>
-        </article>
-    );
-};
-
-const ChatContainer = ({bg_image})=> {
-    const Theme = useContext(ThemeContext);
-    const loadImage = imgCache;
-    
-    loadImage.read(bg_image); //Preload the bg image
-
-    const [request_state,set_request_state] = useState(false); // Determines if the front end is busy requesting or not
-
-    const [chat_blocks,update_chat_blocks] = useState([<Bubble key={0} value={'Izumi-kun eating alone again? *sits next to him*'} response_type={'intro'}/>]);
+   
 
     ws.socket.onmessage = e => {
         const {STATUS} = JSON.parse(e.data);
@@ -224,25 +223,68 @@ const ChatContainer = ({bg_image})=> {
     }
 
     const add_bubble = useCallback((text,response_type) => {
-        const new_block = <Bubble key={chat_blocks.length} set_request_state = {(bool) => set_request_state(bool)} response_type={response_type} value={text}/>;
+        const new_block = <Bubble key={chat_blocks.length} scrollUp={() => ScrollUp()} set_request_state = {(bool) => set_request_state(bool)} response_type={response_type} value={text}/>;
         update_chat_blocks(prev => ([new_block,...prev]));
     },[chat_blocks]);
+
+    return(
+        <>
+            <article ref={ScrollView} className="overflow-y-auto lg:w-3/4 w-full block h-full  ">
+                <div  className="chatContainer w-full h-max rounded-lg p-1 flex flex-col-reverse px-4 gap-8  " style={{background:''}} >
+                    {chat_blocks}
+                </div>
+            </article>
+            <span className=" absolute  bottom lg:w-1/3 md:w-3/4 sm:w-3/4 w-5/6  my-4 border rounded-full flex flex-row px-8  items-center justify-center transform-all" style={{background:Theme.color_layer_1,zIndex:1,opacity:`${request_state ? '0.5' : '1'}`}}>
+                <UserTextArea isRequesting={request_state}  action={(text) => add_bubble(text,'user')} set_request_state = {(bool) => set_request_state(bool)}/>
+           
+            </span>
+        </>
+        
+    );
+};
+
+const ChatContainer = ({bg_image})=> {
+    const Theme = useContext(ThemeContext);
+    const loadImage = imgCache;
+    
+    loadImage.read(bg_image); //Preload the bg image
+
+   
+
+    // const [chat_blocks,update_chat_blocks] = useState([<Bubble key={0} value={'Izumi-kun eating alone again? *sits next to him*'} response_type={'intro'}/>]);
+
+    // ws.socket.onmessage = e => {
+    //     const {STATUS} = JSON.parse(e.data);
+    //     try{
+    //         const {response} = JSON.parse(e.data);
+    //         if(response){
+    //             switch(STATUS){
+    //                 case 200:
+    //                     const {response} = JSON.parse(e.data);
+    //                     add_bubble(response,'ai');
+    //                     break;
+    //                 default:
+    //                     console.error('Error while recieving message');
+    //                     break;
+    //             }
+    //         }
+    //     }catch{
+    //         return;
+    //     }
+        
+    // }
+
+    // const add_bubble = useCallback((text,response_type) => {
+    //     const new_block = <Bubble key={chat_blocks.length} set_request_state = {(bool) => set_request_state(bool)} response_type={response_type} value={text}/>;
+    //     update_chat_blocks(prev => ([new_block,...prev]));
+    // },[chat_blocks]);
 
   
 
     return(
         <article className="w-full h-full flex flex-col-reverse items-center rounded-xl " style={{background:`url(${bg_image}) center/cover no-repeat`}}>
             <div className="absolute w-full h-full pointer-events-none"  style={{background:Theme.DialoguePanelBg}}></div>
-
-            <span className=" absolute  bottom lg:w-1/3 md:w-3/4 sm:w-3/4 w-5/6  my-4 border rounded-full flex flex-row px-8  items-center justify-center transform-all" style={{background:Theme.color_layer_1,zIndex:1,opacity:`${request_state ? '0.5' : '1'}`}}>
-                {/* <Suspense fallback={<p>wait</p>}> */}
-                    <UserTextArea isRequesting={request_state}  action={(text) => add_bubble(text,'user')} set_request_state = {(bool) => set_request_state(bool)}/>
-                {/* </Suspense> */}
-                <SubmitIcon isRequesting={request_state}/>
-            </span>
-
-            <MessageScrollView  chats={chat_blocks}/>
-
+            <MessageScrollView />
         </article> 
     
     );
