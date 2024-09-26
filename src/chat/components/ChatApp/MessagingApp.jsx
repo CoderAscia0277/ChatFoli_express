71b540e5e09127e04f1694330984ff468206694c
CoderAscia0277 <CoderAscia0277@users.noreply.github.com>
2024-09-26 18:13:28 +0800

    That fudgin bug it fixed, iwasn gumamit ng lazy method if ung component ay gusto mo agad mag display without async operatio, lazy() is a asycn function that can trigger the suspense method

--- src/chat/components/ChatApp/MessagingApp.jsx
+++ src/chat/components/ChatApp/MessagingApp.jsx
@@ -2,10 +2,24 @@ import { useContext ,Suspense, useState, useEffect, useRef, memo, useCallback, l
 import { ThemeContext } from "../../..";
 import { MessageAppContext } from "../../core/ChatApp";
 import imgCache from "../../_utils/ImageCache";
-// import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { InitialData } from "../../core/ChatApp";
+import SpinnerIcon from "../Reusable/SpinnerIcon";
 
-const LoadingSpinner = lazy(() => import('../Reusable/SpinnerIcon'));
+
+const localState = createSlice({
+    name:'localState',
+    initialState:{
+        isRequesting:false
+    },reducers:{
+        update_isRequesting:(state,data) => {
+            state.isRequesting = data.payload;
+        }
+    }
+})
+
+const localStore = configureStore({reducer:localState.reducer});
+const {update_isRequesting} = localState.actions;
 
 const ws = {
     socket:null,
@@ -46,32 +60,19 @@ const ws = {
  
 };
 
-
-const SubmitIcon = ({isRequesting,send = () => null}) => {
-    const Theme = useContext(ThemeContext);
-    // const isRequesting = useContext(API_STATUS);
-
-    if(isRequesting){
-        return(<LoadingSpinner size={Theme.IconSize} />)
-    }else{
-        return(
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" onClick={() =>  send()} className={`${Theme.IconSize} hover:scale-110 cursor-pointer`} color="#F8F9FA" fill="none">
-            <path d="M21.0477 3.05293C18.8697 0.707363 2.48648 6.4532 2.50001 8.551C2.51535 10.9299 8.89809 11.6617 10.6672 12.1581C11.7311 12.4565 12.016 12.7625 12.2613 13.8781C13.3723 18.9305 13.9301 21.4435 15.2014 21.4996C17.2278 21.5892 23.1733 5.342 21.0477 3.05293Z" stroke="currentColor" strokeWidth="1.5" />
-            <path d="M11.5 12.5L15 9" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-        </svg>);
-    }
-};
-
-const UserTextArea = ({ action = () => null, set_request_state = () => null, isRequesting}) => {
+const UserTextArea = ({ action = () => null}) => {
 
     const isloaded = useRef(false);
     const text_field = useRef(null);
 
+    const isRequesting = useContext(IsRequestingContext);
+    const Theme = useContext(ThemeContext);
 
     useEffect(() => {
         if(!isloaded.current){
             isloaded.current = true;
             text_field.current.focus();
+            
         }
     },[]);
 
@@ -85,6 +86,9 @@ const UserTextArea = ({ action = () => null, set_request_state = () => null, isR
 
     const submit_action = async(e = null) => {
         let userInput = '';
+        
+        localStore.dispatch(update_isRequesting(true));
+
         if(e != null){
             e.preventDefault();
             if(e.target.value){
@@ -101,25 +105,43 @@ const UserTextArea = ({ action = () => null, set_request_state = () => null, isR
             text_field.current.readOnly = true;
         }
         action(userInput); // creates new user bubble based on input
-        set_request_state(true);
+      
+       
         // send messages to the server      
         ws.socket.send(JSON.stringify({'method':'SEND-MESSAGE','message':userInput}));
-                
+      
     }
 
+    // The problem occurs at the set_requestState and not from thw ws.send or had any relation with the web socket some how the SubmitIcon jsx and the request_state causes
+    // the program to have a sudden async session even though this isn't what intend to which cause the suspense page to trigger automatically causing quick glitch like bug.
+    // In order to fix this I implemented a localState and locat store function and also put the SubmitIcon jsx inside the userTextField jsx 
+    // Ang cause ng error is "lazy(() => import('../Reusable/SpinnerIcon'));" lazy is an async method kaya once first time mo sya tawagin may promise na mangyayari 
+    // In short nag kakaroon ng delay ung spinner icon kaya may promise, to fix this import mo nlng ung spinner Icon without lazy
+   
     return(
         <>
             <input type="text"  ref={text_field} onKeyDown={e => e.key === "Enter" ? submit_action(e) : null} className="bg-transparent  flex-grow h-14 outline-0 px-4 text-center text-neutral-100"  placeholder={`${isRequesting ? "Azumi is currently typing..." :"Please enter your response here."}`} style={{resize:'none'}}/>      
-            <SubmitIcon send={() => submit_action()} isRequesting={isRequesting}/>
+            
+            {
+                isRequesting ?
+                    (<SpinnerIcon size={Theme.IconSize} />)
+                :
+                    ( //Submit Icon
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" onClick={() =>  submit_action()} className={`${Theme.IconSize} hover:scale-110 cursor-pointer`} color="#F8F9FA" fill="none">
+                            <path d="M21.0477 3.05293C18.8697 0.707363 2.48648 6.4532 2.50001 8.551C2.51535 10.9299 8.89809 11.6617 10.6672 12.1581C11.7311 12.4565 12.016 12.7625 12.2613 13.8781C13.3723 18.9305 13.9301 21.4435 15.2014 21.4996C17.2278 21.5892 23.1733 5.342 21.0477 3.05293Z" stroke="currentColor" strokeWidth="1.5" />
+                            <path d="M11.5 12.5L15 9" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                    )
+                
+            }
         </>
        
     );
 };
 
-const Bubble = ({scrollUp = () => null,value,response_type,set_request_state = () => null}) => {
+const Bubble = ({scrollUp = () => null,value,response_type}) => {
     const Theme = useContext(ThemeContext);
     const [streamMessage,update_streamMessage] = useState('');
-    // const text_field = useRef(null);
 
     const isloaded = useRef(false);
 
@@ -142,7 +164,9 @@ const Bubble = ({scrollUp = () => null,value,response_type,set_request_state = (
 
                         if(this.cache.length === text_array.length){
                             clearInterval(interval);
-                            set_request_state(false);
+
+                            //Set isRequesting to false, it means its done responding
+                            localStore.dispatch(update_isRequesting(false));
                         }
                     },30);
                 }
@@ -162,7 +186,7 @@ const Bubble = ({scrollUp = () => null,value,response_type,set_request_state = (
             </div> 
            
         );
-    }else if(response_type == 'intro'){
+    }else if(response_type === 'intro'){
         return(
             <div className={`chatBubble  fading w-full h-max flex flex-row  justify-start`} style={{pointerEvents:'none'}}>
                  <span className="w-max dialouge_wrap  h-max min-h-10 rounded-2xl border  px-4 py-2 break-normal" style={{flexShrink:0,background:Theme.color_layer_2,color:'#F8F9FA',overflowWrap: 'normal',wordBreak:'normal'}}>{value}</span>
@@ -182,11 +206,17 @@ const Bubble = ({scrollUp = () => null,value,response_type,set_request_state = (
     
 };
 
+const IsRequestingContext = createContext();
+
 const MessageScrollView = () => {
 
     const Theme = useContext(ThemeContext);
 
-    const [request_state,set_request_state] = useState(false); // Determines if the front end is busy requesting or not
+    const [request_state,set_request_state] = useState(localStore.getState().isRequesting); // Determines if the front end is busy requesting or not
+    
+    localStore.subscribe(() => {
+        set_request_state(localStore.getState().isRequesting);
+    });
 
     const ScrollUp = () => {
         ScrollView.current.scrollTop = ScrollView.current.scrollHeight;
@@ -223,22 +253,22 @@ const MessageScrollView = () => {
     }
 
     const add_bubble = useCallback((text,response_type) => {
-        const new_block = <Bubble key={chat_blocks.length} scrollUp={() => ScrollUp()} set_request_state = {(bool) => set_request_state(bool)} response_type={response_type} value={text}/>;
+        const new_block = <Bubble key={chat_blocks.length} scrollUp={() => ScrollUp()} response_type={response_type} value={text}/>;
         update_chat_blocks(prev => ([new_block,...prev]));
     },[chat_blocks]);
 
     return(
-        <>
+        <IsRequestingContext.Provider value={request_state}>
             <article ref={ScrollView} className="overflow-y-auto lg:w-3/4 w-full block h-full  ">
                 <div  className="chatContainer w-full h-max rounded-lg p-1 flex flex-col-reverse px-4 gap-8  " style={{background:''}} >
                     {chat_blocks}
                 </div>
             </article>
             <span className=" absolute  bottom lg:w-1/3 md:w-3/4 sm:w-3/4 w-5/6  my-4 border rounded-full flex flex-row px-8  items-center justify-center transform-all" style={{background:Theme.color_layer_1,zIndex:1,opacity:`${request_state ? '0.5' : '1'}`}}>
-                <UserTextArea isRequesting={request_state}  action={(text) => add_bubble(text,'user')} set_request_state = {(bool) => set_request_state(bool)}/>
+                <UserTextArea action={(text) => add_bubble(text,'user')}/>
            
             </span>
-        </>
+        </IsRequestingContext.Provider>
         
     );
 };
@@ -248,37 +278,6 @@ const ChatContainer = ({bg_image})=> {
     const loadImage = imgCache;
     
     loadImage.read(bg_image); //Preload the bg image
-
-   
-
-    // const [chat_blocks,update_chat_blocks] = useState([<Bubble key={0} value={'Izumi-kun eating alone again? *sits next to him*'} response_type={'intro'}/>]);
-
-    // ws.socket.onmessage = e => {
-    //     const {STATUS} = JSON.parse(e.data);
-    //     try{
-    //         const {response} = JSON.parse(e.data);
-    //         if(response){
-    //             switch(STATUS){
-    //                 case 200:
-    //                     const {response} = JSON.parse(e.data);
-    //                     add_bubble(response,'ai');
-    //                     break;
-    //                 default:
-    //                     console.error('Error while recieving message');
-    //                     break;
-    //             }
-    //         }
-    //     }catch{
-    //         return;
-    //     }
-        
-    // }
-
-    // const add_bubble = useCallback((text,response_type) => {
-    //     const new_block = <Bubble key={chat_blocks.length} set_request_state = {(bool) => set_request_state(bool)} response_type={response_type} value={text}/>;
-    //     update_chat_blocks(prev => ([new_block,...prev]));
-    // },[chat_blocks]);
-
   
 
     return(
